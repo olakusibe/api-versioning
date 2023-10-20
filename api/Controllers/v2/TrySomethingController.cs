c7430227d3d07fa7c71b2786da00305bb468c339
Olakusibe Aremu-Oluwole <olashod@gmail.com>
2023-10-20 04:56:22 +0100

    Add project files.

---
+++ api/Controllers/v2/TrySomethingController.cs
@@ -0,0 +1,27 @@
+﻿using api.Models;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers.v2
+{
+    [ApiController]
+    [ApiVersion("2")]
+    [ApiExplorerSettings(GroupName = "v2")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class TrySomethingController : ControllerBase
+    {
+        private readonly ILogger<TrySomethingController> _logger;
+
+        public TrySomethingController(ILogger<TrySomethingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Method([FromBody] Request request)
+        {
+            return await Task.FromResult(StatusCode(200, "Hello v2"));
+        }
+    }
+}
---
+++ api/Program.cs
@@ -0,0 +1,66 @@
+using Asp.Versioning;
+using Asp.Versioning.Conventions;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Read up
+// https://github.com/dotnet/aspnet-api-versioning/issues/929
+// https://github.com/dotnet/aspnet-api-versioning/wiki/API-Version-Conventions#version-by-namespace-convention
+// https://code-maze.com/aspnetcore-api-versioning/#:~:text=as%20%E2%80%9C%E2%80%98v%E2%80%99major%5B.minor%5D%5B%2Dstatus%5D%E2%80%9D.
+
+
+// Add services to the container.
+builder.Services.AddApiVersioning(opts =>
+{
+    opts.ReportApiVersions = true;
+    opts.ApiVersionReader = new UrlSegmentApiVersionReader();
+    opts.DefaultApiVersion = new ApiVersion(1);
+    opts.AssumeDefaultVersionWhenUnspecified = true;
+})
+    .AddMvc(opts =>
+    {
+        opts.Conventions.Add(new VersionByNamespaceConvention());
+        //opts.Conventions.Controller<api.Controllers.v1.TrySomethingController>().HasApiVersion(new Asp.Versioning.ApiVersion(1));
+        //opts.Conventions.Controller<api.Controllers.v2.TrySomethingController>().HasApiVersion(new Asp.Versioning.ApiVersion(2));
+        //opts.Conventions.Controller<api.Controllers.WeatherForecastController>().HasApiVersion(new Asp.Versioning.ApiVersion(1));
+    })
+    .AddApiExplorer(opts => 
+    {
+        opts.GroupNameFormat = "'v'V"; // using "'v'VVV" formart means “‘v’major[.minor][-status]”
+        opts.SubstituteApiVersionInUrl = true;
+    });
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo() { Title = "API v1", Version = "v1" });
+    opts.SwaggerDoc("v2", new OpenApiInfo() { Title = "API v2", Version = "v2" });
+    opts.ResolveConflictingActions(apiDesc => apiDesc.First());
+    opts.CustomSchemaIds(x => x.FullName);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(opts =>
+    {
+        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        opts.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
